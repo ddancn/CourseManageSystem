a409ece241f580a794993eed547375aa34e114d8
18859279003 <34016971+18859279003@users.noreply.github.com>
2017-12-06 10:44:27 +0800

    Add files via upload

--- src/main/java/xmu/crms/view/StudentController.java
+++ src/main/java/xmu/crms/view/StudentController.java
@@ -22,71 +22,71 @@ public class StudentController {
 	//学生绑定
     @RequestMapping(value="/StudentBindPage", method=RequestMethod.GET)
     void StudentBindPage(HttpServletResponse response) throws IOException {
-    	response.sendRedirect("StudentBindPage.html");
+    	response.sendRedirect("html/student/StudentBindPage.html");
 	}
   //学生选课
     @RequestMapping(value="/StudentChooseCoursePage", method=RequestMethod.GET)
     void StudentChooseCoursePage(HttpServletResponse response) throws IOException {
-    	response.sendRedirect("StudentChooseCoursePage.html");
+    	response.sendRedirect("html/student/StudentChooseCoursePage.html");
 	}
   //学生课程首页
     @RequestMapping(value="/StudentCourseHome", method=RequestMethod.GET)
     void StudentCourseHome(HttpServletResponse response) throws IOException {
-    	response.sendRedirect("StudentCourseHome.html");
+    	response.sendRedirect("html/student/StudentCourseHome.html");
 	}
   //学生课程信息
     @RequestMapping(value="/StudentCourseInformation", method=RequestMethod.GET)
     void StudentCourseInformation(HttpServletResponse response) throws IOException {
-    	response.sendRedirect("StudentCourseInformation.html");
+    	response.sendRedirect("html/student/StudentCourseInformation.html");
 	}
     //学生固定分组讨论课
     @RequestMapping(value="/StudentSeminarPage(fixed)", method=RequestMethod.GET)
     void StudentFixedSemunarPage(HttpServletResponse response) throws IOException {
-    	response.sendRedirect("StudentFixedSeminarPage(fixed).html");
+    	response.sendRedirect("html/student/StudentFixedSeminarPage(fixed).html");
 	}
   //学生个人信息首页
     @RequestMapping(value="/StudentHomePage", method=RequestMethod.GET)
     void StudentHomePage(HttpServletResponse response) throws IOException {
-    	response.sendRedirect("StudentHomePage.html");
+    	response.sendRedirect("html/student/StudentHomePage.html");
 	}
     //学生个人信息修改
     @RequestMapping(value="/StudentInfoModifyPage", method=RequestMethod.GET)
     void StudentInfoModifyPage(HttpServletResponse response) throws IOException {
-    	response.sendRedirect("StudentInfoModifyPage.html");
+    	response.sendRedirect("html/student/StudentInfoModifyPage.html");
 	}
     //学生管理分组
     @RequestMapping(value="/StudentManageGroupPage", method=RequestMethod.GET)
     void StudentManageGroupPage(HttpServletResponse response) throws IOException {
-    	response.sendRedirect("StudentManageGroupPage.html");
+    	response.sendRedirect("html/student/StudentManageGroupPage.html");
 	}
     //学生修改分组
     @RequestMapping(value="/StudentModifyGroupPage", method=RequestMethod.GET)
     void StudentModifyGroupPage(HttpServletResponse response) throws IOException {
-    	response.sendRedirect("StudentModifyGroupPage.html");
+    	response.sendRedirect("html/student/StudentModifyGroupPage.html");
 	}
     //学生随机讨论课
     @RequestMapping(value="/StudentSeminarPage(random)", method=RequestMethod.GET)
     void StudentRandomSeminarPage(HttpServletResponse response) throws IOException {
-    	response.sendRedirect("StudenSeminarPage(random).html");
+    	response.sendRedirect("html/student/StudenSeminarPage(random).html");
 	}
   //学生查看分数
     @RequestMapping(value="/StudentViewGradePage", method=RequestMethod.GET)
     void StudentViewGradePage(HttpServletResponse response) throws IOException {
-    	response.sendRedirect("StudenViewGradePage.html");
+    	response.sendRedirect("html/student/StudenViewGradePage.html");
 	}
   //学生查看分组
     @RequestMapping(value="/StudentViewGroupPage", method=RequestMethod.GET)
     void StudentViewGroupPage(HttpServletResponse response) throws IOException {
-    	response.sendRedirect("StudenViewGroupPage.html");
+    	response.sendRedirect("html/student/StudenViewGroupPage.html");
 	}
     //学生查看话题（固定）
     @RequestMapping(value="/StudentViewTopicPage(fixed)", method=RequestMethod.GET)
     void StudentViewFixedTopicPage(HttpServletResponse response) throws IOException {
-    	response.sendRedirect("StudenViewTopicPage(fixed).html");
+    	response.sendRedirect("html/student/StudenViewTopicPage(fixed).html");
 	}
     //学生查看话题（随机）
     @RequestMapping(value="/StudentViewTopicPage(random)", method=RequestMethod.GET)
     void StudentViewRandomTopicPage(HttpServletResponse response) throws IOException {
-    	response.sendRedirect("StudenViewTopicPage(random).html");
+    	response.sendRedirect("html/student/StudenViewTopicPage(random).html");
 	}
 }
--- src/main/java/xmu/crms/view/TeacherController.java
+++ src/main/java/xmu/crms/view/TeacherController.java
@@ -22,111 +22,111 @@ public class TeacherController {
 	//微信登陆
     @RequestMapping(value="/WechatLoginPage", method=RequestMethod.GET)
     void Wechat(HttpServletResponse response) throws IOException {
-    	response.sendRedirect("WechatLoginPage.html");
+    	response.sendRedirect("html/teacher/WechatLoginPage.html");
 	}
   //账号密码登录
     @RequestMapping(value="/AccountLoginPage", method=RequestMethod.GET)
     void Account(HttpServletResponse response) throws IOException {
-    	response.sendRedirect("AccountLoginPage.html");
+    	response.sendRedirect("html/teacher/AccountLoginPage.html");
 	}
     //注册
     @RequestMapping(value="/RegisterPage", method=RequestMethod.GET)
     void Register(HttpServletResponse response) throws IOException {
-    	response.sendRedirect("RegisterPage.html");
+    	response.sendRedirect("html/teacher/RegisterPage.html");
 	}
     //老师绑定
     @RequestMapping(value="/TeacherBindPage", method=RequestMethod.GET)
     void TeacherBind(HttpServletResponse response) throws IOException {
-    	response.sendRedirect("TeacherBindPage.html");
+    	response.sendRedirect("html/teacher/TeacherBindPage.html");
 	}
     //老师班级信息
     @RequestMapping(value="/TeacherClassInfo", method=RequestMethod.GET)
     void TeacherClassInfo(HttpServletResponse response) throws IOException {
-    	response.sendRedirect("TeacherClassInfo.html");
+    	response.sendRedirect("html/teacher/TeacherClassInfo.html");
 	}
   //老师课程首页
     @RequestMapping(value="/TeacherCourseHomePage", method=RequestMethod.GET)
     void TeacherCourseHome(HttpServletResponse response) throws IOException {
-    	response.sendRedirect("TeacherCourseHomePage.html");
+    	response.sendRedirect("html/teacher/TeacherCourseHomePage.html");
 	}
     //老师课程信息
     @RequestMapping(value="/TeacherCourseInformation", method=RequestMethod.GET)
     void TeacherCourseInformation(HttpServletResponse response) throws IOException {
-    	response.sendRedirect("TeacherCourseInformation.html");
+    	response.sendRedirect("html/teacher/TeacherCourseInformation.html");
 	}
     //老师创建班级
     @RequestMapping(value="/TeacherCreateClass", method=RequestMethod.GET)
     void TeacherCreateClass(HttpServletResponse response) throws IOException {
-    	response.sendRedirect("TeacherCreateClass.html");
+    	response.sendRedirect("html/teacher/TeacherCreateClass.html");
 	}
   //老师创建课程
     @RequestMapping(value="/TeacherCreateCoursePage", method=RequestMethod.GET)
     void TeacherCreateCourse(HttpServletResponse response) throws IOException {
-    	response.sendRedirect("TeacherCreateCoursePage.html");
+    	response.sendRedirect("html/teacher/TeacherCreateCoursePage.html");
 	}
     //老师创建学校
     @RequestMapping(value="/TeacherCreateSchool", method=RequestMethod.GET)
     void TeacherCreateSchool(HttpServletResponse response) throws IOException {
-    	response.sendRedirect("TeacherCreateSchool.html");
+    	response.sendRedirect("html/teacher/TeacherCreateSchool.html");
 	}
   //老师创建讨论课
     @RequestMapping(value="/TeacherCreateSeminar", method=RequestMethod.GET)
     void TeacherCreateSeminar(HttpServletResponse response) throws IOException {
-    	response.sendRedirect("TeacherCreateSeminar.html");
+    	response.sendRedirect("html/teacher/TeacherCreateSeminar.html");
 	}
     //老师创建课题
     @RequestMapping(value="/TeacherCreateTopic", method=RequestMethod.GET)
     void TeacherCreateTopic(HttpServletResponse response) throws IOException {
-    	response.sendRedirect("TeacherCreateTopic.html");
+    	response.sendRedirect("html/teacher/TeacherCreateTopic.html");
 	}
   //老师个人信息主页
     @RequestMapping(value="/TeacherHomePage", method=RequestMethod.GET)
     void TeacherHomePage(HttpServletResponse response) throws IOException {
-    	response.sendRedirect("TeacherHomePage.html");
+    	response.sendRedirect("html/teacher/TeacherHomePage.html");
 	}
     //老师个人信息修改
     @RequestMapping(value="/TeacherInfoModifyPage", method=RequestMethod.GET)
     void TeacherInfoModifyPage(HttpServletResponse response) throws IOException {
-    	response.sendRedirect("TeacherInfoModifyPage.html");
+    	response.sendRedirect("html/teacher/TeacherInfoModifyPage.html");
 	}
   //老师打分首页
     @RequestMapping(value="/TeacherScoreHome", method=RequestMethod.GET)
     void TeacherScoreHome(HttpServletResponse response) throws IOException {
-    	response.sendRedirect("TeacherScoreHome.html");
+    	response.sendRedirect("html/teacher/TeacherScoreHome.html");
 	}
     //老师审阅打分首页
     @RequestMapping(value="/TeacherScoreReportPage", method=RequestMethod.GET)
     void TeacherScoreReportPage(HttpServletResponse response) throws IOException {
-    	response.sendRedirect("TeacherScoreReportPage.html");
+    	response.sendRedirect("html/teacher/TeacherScoreReportPage.html");
 	}
     //老师讨论课信息
     @RequestMapping(value="/TeacherSenimarInfo", method=RequestMethod.GET)
     void TeacherSeminarInfo(HttpServletResponse response) throws IOException {
-    	response.sendRedirect("TeacherSenimarInfo.html");
+    	response.sendRedirect("html/teacher/TeacherSenimarInfo.html");
 	}
     //老师讨论课课后查询话题
     @RequestMapping(value="/TeacherTopicCheckAfter", method=RequestMethod.GET)
     void TeacherTopicCheckAfter(HttpServletResponse response) throws IOException {
-    	response.sendRedirect("TeacherTopicCheckAfter.html");
+    	response.sendRedirect("html/teacher/TeacherTopicCheckAfter.html");
 	}
     //老师讨论课课前查询话题
     @RequestMapping(value="/TeacherTopicCheckBefore", method=RequestMethod.GET)
     void TeacherTopicCheckBefore(HttpServletResponse response) throws IOException {
-    	response.sendRedirect("TeacherTopicCheckBefore.html");
+    	response.sendRedirect("html/teacher/TeacherTopicCheckBefore.html");
 	}
   //老师修改班级
     @RequestMapping(value="/TeacherUpdateClass", method=RequestMethod.GET)
     void TeacherUpdateClass(HttpServletResponse response) throws IOException {
-    	response.sendRedirect("TeacherUpdateClass.html");
+    	response.sendRedirect("html/teacher/TeacherUpdateClass.html");
 	}
   //老师修改讨论课
     @RequestMapping(value="/TeacherUpdateSeminar", method=RequestMethod.GET)
     void TeacherUpdateSeminar(HttpServletResponse response) throws IOException {
-    	response.sendRedirect("TeacherUpdateSeminar.html");
+    	response.sendRedirect("html/teacher/TeacherUpdateSeminar.html");
 	}
   //老师修改课题
     @RequestMapping(value="/TeacherUpdateTopic", method=RequestMethod.GET)
     void TeacherUpdateTopic(HttpServletResponse response) throws IOException {
-    	response.sendRedirect("TeacherUpdateTopic.html");
+    	response.sendRedirect("html/teacher/TeacherUpdateTopic.html");
 	}
 }
