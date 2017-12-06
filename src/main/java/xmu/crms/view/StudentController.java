7097bb54d5725c831c468a0321cd4399226580fb
ddancn <33754060+ddancn@users.noreply.github.com>
2017-12-06 20:39:20 +0800

    Update StudentController.java

--- src/main/java/xmu/crms/view/StudentController.java
+++ src/main/java/xmu/crms/view/StudentController.java
@@ -11,13 +11,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.Controller;
 
 
 import xmu.crms.model.Student;
 
-@RestController
-@RequestMapping("/Student")
+@Controller
+@RequestMapping
 public class StudentController {
 	//学生绑定
     @RequestMapping(value="/StudentBindPage", method=RequestMethod.GET)
