2d8b84c6db098b795863de2debef4de2da7b516f
ddancn <33754060+ddancn@users.noreply.github.com>
2017-12-06 21:50:30 +0800

    Update StudentController.java

--- src/main/java/xmu/crms/view/StudentController.java
+++ src/main/java/xmu/crms/view/StudentController.java
@@ -1,20 +1,11 @@
 package xmu.crms.view;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 import javax.servlet.http.HttpServletResponse;
-import javax.websocket.server.PathParam;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.Controller;
-
-
-import xmu.crms.model.Student;
 
 @Controller
 @RequestMapping
