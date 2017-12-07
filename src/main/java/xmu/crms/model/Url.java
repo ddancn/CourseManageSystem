13662e2e890ab8a165783b23e5d2ea6fa55426d1
ddancn <33754060+ddancn@users.noreply.github.com>
2017-12-07 11:37:08 +0800

    Add url.java

---
+++ src/main/java/xmu/crms/model/Url.java
@@ -0,0 +1,23 @@
+package xmu.crms.model;
+
+public class Url {
+
+    private String url;
+    
+    public Url(){
+        
+    }
+    
+    public Url(String url) {
+        super();
+        this.url = url;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
